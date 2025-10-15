9e42a39d6090bd4326c39695170bca2924ba0105
Running-Lag <131605267+Running-Lag@users.noreply.github.com>
2025-10-15 13:06:02 +0200

    Used cargo fmt to use the official rust formatting style

--- compiler/error/src/lib.rs
+++ compiler/error/src/lib.rs
@@ -1,8 +1,8 @@
 #![forbid(unsafe_code)]
 
+use colored::{Color, Colorize};
 use std::fmt::Debug;
 use std::ops::Add;
-use colored::{Color, Colorize};
 
 const ERROR_CONTEXT_LINES: usize = 3;
 /** A syntax error
@@ -10,21 +10,23 @@ This struct is used for storing and displaying syntax errors
 */
 #[derive(Debug)]
 
-pub struct SyntaxError
-{
+pub struct SyntaxError {
     area: CodeArea,
     file_location: String,
-    error_type: Box<dyn ErrorType>
+    error_type: Box<dyn ErrorType>,
 }
 
-impl SyntaxError
-{
+impl SyntaxError {
     /** Creates a new syntax error with the specified fields
        Only intended for internal use.
        To create a SyntaxError elsewhere, use a [`SyntaxErrorBuilder`]
     */
     fn new(area: CodeArea, file_location: String, error_type: Box<dyn ErrorType>) -> Self {
-        Self { area, file_location, error_type }
+        Self {
+            area,
+            file_location,
+            error_type,
+        }
     }
 }
 
@@ -33,11 +35,10 @@ Expects the user to set every field before building
 */
 #[derive(Debug)]
 
-pub struct SyntaxErrorBuilder
-{
+pub struct SyntaxErrorBuilder {
     area: Option<CodeArea>,
     file_location: Option<String>,
-    error_type: Option<Box<dyn ErrorType>>
+    error_type: Option<Box<dyn ErrorType>>,
 }
 
 impl Default for SyntaxErrorBuilder {
@@ -46,18 +47,20 @@ impl Default for SyntaxErrorBuilder {
     }
 }
 
-impl SyntaxErrorBuilder
-{
+impl SyntaxErrorBuilder {
     /** Creates a new and empty [`SyntaxErrorBuilder`]
-    */
+     */
     pub fn new() -> Self {
-        Self { area: None, file_location: None, error_type: None }
+        Self {
+            area: None,
+            file_location: None,
+            error_type: None,
+        }
     }
 
     /** Sets an error area
-    */
-    pub fn with_area(mut self, area: CodeArea) -> Self
-    {
+     */
+    pub fn with_area(mut self, area: CodeArea) -> Self {
         self.area = Some(area);
         self
     }
@@ -65,16 +68,14 @@ impl SyntaxErrorBuilder
     /** Sets the path of the file that contains the erroneous code
        This is for display purposes only
     */
-    pub fn with_file_location(mut self, file_location: String) -> Self
-    {
+    pub fn with_file_location(mut self, file_location: String) -> Self {
         self.file_location = Some(file_location);
         self
     }
 
     /** Sets the type of error
-    */
-    pub fn with_error_type(mut self, error_type: impl ErrorType+'static) -> Self
-    {
+     */
+    pub fn with_error_type(mut self, error_type: impl ErrorType + 'static) -> Self {
         self.error_type = Some(Box::new(error_type));
         self
     }
@@ -83,17 +84,15 @@ impl SyntaxErrorBuilder
     # Panics
     Panics if not all fields are set
     */
-    pub fn build(self) -> SyntaxError
-    {
-        if self.file_location.is_none() ||
-            self.error_type.is_none() ||
-            self.area.is_none()
-        {
+    pub fn build(self) -> SyntaxError {
+        if self.file_location.is_none() || self.error_type.is_none() || self.area.is_none() {
             panic!("Not all fields are set!");
         }
-        SyntaxError::new(self.area.unwrap(),
-                                     self.file_location.unwrap(),
-                                     self.error_type.unwrap())
+        SyntaxError::new(
+            self.area.unwrap(),
+            self.file_location.unwrap(),
+            self.error_type.unwrap(),
+        )
     }
 }
 
@@ -102,14 +101,12 @@ Identified by line and char.
 Both line and char are zero-based
 */
 #[derive(Debug, PartialEq)]
-pub struct CodeLocation
-{
+pub struct CodeLocation {
     line: usize,
-    char: usize
+    char: usize,
 }
 
-impl CodeLocation
-{
+impl CodeLocation {
     pub fn new(line: usize, char: usize) -> Self {
         Self { line, char }
     }
@@ -128,22 +125,18 @@ The start is inclusive
 The line of the end is inclusive, the char exclusive
  */
 #[derive(Debug, PartialEq)]
-pub struct CodeArea
-{
+pub struct CodeArea {
     start: CodeLocation,
-    end: CodeLocation
+    end: CodeLocation,
 }
 
-impl CodeArea
-{
+impl CodeArea {
     /** Creates a new [`CodeArea`]
        # Panics
        Panics if start is not before end
     */
     pub fn new(start: CodeLocation, end: CodeLocation) -> Option<Self> {
-        if start.line > end.line
-            || (start.line == end.line && start.char >= end.char)
-        {
+        if start.line > end.line || (start.line == end.line && start.char >= end.char) {
             return None;
         }
         Some(Self { start, end })
@@ -164,73 +157,90 @@ const ERROR_CODE_COLOR: Color = Color::TrueColor {
     b: 0,
 };
 
-impl SyntaxError
-{
+impl SyntaxError {
     /** Prints the error to stdout
     # Panic
     Will panic if the area is referencing to locations that don't exist in the the provided code
     */
-    pub fn print(&self, code: &str)
-    {
+    pub fn print(&self, code: &str) {
         // Prints Error and the error message
         eprintln!("{} {}", "Error:".red(), self.error_type.to_string());
         eprintln!();
-        eprintln!("{}{}{}", "[".bright_black().bold(), self.file_location.bright_black().bold(), "]".bright_black().bold());
+        eprintln!(
+            "{}{}{}",
+            "[".bright_black().bold(),
+            self.file_location.bright_black().bold(),
+            "]".bright_black().bold()
+        );
 
         // The start and end
         let error_start_line = self.area.start.line();
         let error_end_line = self.area.end.line();
         // The start and end including the padding
-        let display_end = error_end_line+ERROR_CONTEXT_LINES;
-        let display_start = if error_start_line < ERROR_CONTEXT_LINES {0} else {error_start_line-ERROR_CONTEXT_LINES};
+        let display_end = error_end_line + ERROR_CONTEXT_LINES;
+        let display_start = if error_start_line < ERROR_CONTEXT_LINES {
+            0
+        } else {
+            error_start_line - ERROR_CONTEXT_LINES
+        };
         // Print the codelines
-        for (index, line) in code.lines().enumerate()
-        {
+        for (index, line) in code.lines().enumerate() {
             // If the code line is out of the window to print, skip it
-            if index < display_start || index > display_end
-            {
+            if index < display_start || index > display_end {
                 continue;
             }
 
             // Where the error begins and where it ends in the current line
             // Used for making the text white or yellow
             #[allow(clippy::comparison_chain)]
-            let error_start_char =
-                if index < error_start_line {line.len()} //Before the error lines, so it doesn't begin at all
-                else if index == error_start_line {self.area.start.char()}
-                else {0};
+            let error_start_char = if index < error_start_line {
+                line.len()
+            }
+            //Before the error lines, so it doesn't begin at all
+            else if index == error_start_line {
+                self.area.start.char()
+            } else {
+                0
+            };
 
             // Using a match makes this hard to read
             #[allow(clippy::comparison_chain)]
-            let error_end_char =
-                if index > error_end_line {0} //After the error lines, so it end immediately
-                else if index == error_end_line {self.area.end.char()}
-                else {line.len()};
+            let error_end_char = if index > error_end_line {
+                0
+            }
+            //After the error lines, so it end immediately
+            else if index == error_end_line {
+                self.area.end.char()
+            } else {
+                line.len()
+            };
 
             // Print the line
             // The line is zero-based
-            eprint!("{}", (index+1).to_string().add(": \t").bright_blue().bold());
-            if error_start_char != 0 // If the error starts at the beginning of the line, don't include a white portion
+            eprint!(
+                "{}",
+                (index + 1).to_string().add(": \t").bright_blue().bold()
+            );
+            if error_start_char != 0
+            // If the error starts at the beginning of the line, don't include a white portion
             {
                 eprint!("{}", &line[0..error_start_char]);
             }
-            eprint!("{}", &line[error_start_char..error_end_char].color(ERROR_CODE_COLOR));
-            if error_end_char != line.len()
-            {
+            eprint!(
+                "{}",
+                &line[error_start_char..error_end_char].color(ERROR_CODE_COLOR)
+            );
+            if error_end_char != line.len() {
                 eprint!("{}", &line[error_end_char..line.len()]);
             }
             eprintln!()
         }
-
     }
 }
 
-pub trait ErrorType: Debug
-{
+pub trait ErrorType: Debug {
     fn to_string(&self) -> String;
-
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -238,17 +248,18 @@ mod tests {
 
     #[derive(Debug)]
     pub struct ExampleError(String);
-    impl ErrorType for ExampleError
-    {
+    impl ErrorType for ExampleError {
         fn to_string(&self) -> String {
-            format!("\"{}\" is an invalid data type at this point in the programm", self.0)
+            format!(
+                "\"{}\" is an invalid data type at this point in the programm",
+                self.0
+            )
         }
     }
     #[test]
-    fn error()
-    {
+    fn error() {
         let error = SyntaxErrorBuilder::new()
-            .with_area(CodeArea::new(CodeLocation::new(14,10), CodeLocation::new(14,18)).unwrap())
+            .with_area(CodeArea::new(CodeLocation::new(13, 10), CodeLocation::new(13, 18)).unwrap())
             .with_error_type(ExampleError("CodeArea".to_string()))
             .with_file_location("main.waso".to_string())
             .build();
@@ -256,15 +267,12 @@ mod tests {
         error.print(include_str!("lib.rs"));
     }
 
-    fn codearea_invalid_should_return_none()
-    {
-        let codearea = CodeArea::new(CodeLocation::new(10,10),
-        CodeLocation::new(5,10));
+    fn codearea_invalid_should_return_none() {
+        let codearea = CodeArea::new(CodeLocation::new(10, 10), CodeLocation::new(5, 10));
 
         assert_eq!(None, codearea);
 
-        let codearea = CodeArea::new(CodeLocation::new(10,10),
-                                     CodeLocation::new(10,10));
+        let codearea = CodeArea::new(CodeLocation::new(10, 10), CodeLocation::new(10, 10));
 
         assert_eq!(None, codearea);
     }
