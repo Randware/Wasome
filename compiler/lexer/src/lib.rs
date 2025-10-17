06878133e52da2479d97f760604edf9c2b1b660a
Luis <luis.neuhuber@gmail.com>
2025-10-17 08:06:28 +0200

    Update compiler/lexer/src/lib.rs
    Co-authored-by: Running_Lag <131605267+Running-Lag@users.noreply.github.com>

--- compiler/lexer/src/lib.rs
+++ compiler/lexer/src/lib.rs
@@ -7,17 +7,16 @@ pub use tokens::TokenType;
 pub fn lex(input: &str) -> impl Iterator<Item = Result<Token, LexError>> {
     let mut lexer = TokenType::lexer(input);
 
-    std::iter::from_fn(move || match lexer.next() {
-        Some(Ok(kind)) => {
+std::iter::from_fn(move || Some(match lexer.next()? {
+        Ok(kind) => {
             let line = lexer.extras.0;
             let span = {
                 let start = lexer.span().start.saturating_sub(lexer.extras.1.start);
                 let end = lexer.span().end.saturating_sub(lexer.extras.1.start);
                 start..end
             };
-            Some(Ok(Token { kind, line, span }))
+            Ok(Token { kind, line, span })
         }
-        Some(Err(e)) => Some(Err(e)),
-        None => None,
-    })
+        Err(e) => Err(e)
+    }))
 }
