07a3323bc7008721f44c80c6a10acb8f054a6684
Running-Lag <131605267+Running-Lag@users.noreply.github.com>
2025-10-20 13:34:45 +0200

    Removed the panics when a field in the error builder is missing. Also changed the error type to an enum

--- compiler/error/src/lib.rs
+++ compiler/error/src/lib.rs
@@ -12,7 +12,9 @@ This struct is used for storing and displaying syntax errors
 pub struct SyntaxError {
     area: CodeArea,
     file_location: String,
-    error_type: Box<dyn ErrorType>,
+    error_type: ErrorType,
+    error_msg: String,
+    code: String
 }
 
 impl SyntaxError {
@@ -20,16 +22,131 @@ impl SyntaxError {
        Only intended for internal use.
        To create a SyntaxError elsewhere, use a [`SyntaxErrorBuilder`]
     */
-    fn new(area: CodeArea, file_location: String, error_type: Box<dyn ErrorType>) -> Self {
+    fn new(area: CodeArea, file_location: String, error_type: ErrorType, error_msg: String, code: String) -> Self {
         Self {
             area,
             file_location,
             error_type,
+            error_msg,
+            code
         }
     }
 
-    pub fn builder() -> SyntaxErrorBuilder<NotSet, NotSet, NotSet> {
+    pub fn builder() -> SyntaxErrorBuilder<NotSet, NotSet, NotSet, NotSet, NotSet> {
         SyntaxErrorBuilder::new()
+    }
+
+    /** Prints the error to stdout
+       # Panic
+       May panic if the area is referencing to locations that don't exist in the the provided code
+    */
+    pub fn print(&self) {
+        // Orange
+        let error_code_color = Color::BrightWhite;
+        let error_msg_color = Color::Red;
+        let syntax_error_color = Color::BrightWhite;
+        let code_color = Color::Primary;
+
+        let mut line_starting_pos = 0;
+        // The start and end
+        let error_start_line = self.area.start().line();
+        let error_end_line = self.area.end().line();
+        // The start and end including the padding
+        let display_end = error_end_line + ERROR_CONTEXT_LINES;
+        let display_start = if error_start_line < ERROR_CONTEXT_LINES {
+            0
+        } else {
+            error_start_line - ERROR_CONTEXT_LINES
+        };
+
+        // The processed lines with annotations
+        let mut lines = Vec::new();
+        // Where the error begins, relative to the start of code
+        let mut error_start_char = 0;
+        let mut error_end_char = 0;
+        // Process the codelines
+        for (line_num, line) in self.code.lines().enumerate() {
+            // If the code line is out of the window to print, skip it
+            if line_num < display_start || line_num > display_end {
+                //
+                Self::update_line_starting_pos(&self.code, &mut line_starting_pos);
+                continue;
+            }
+            // Update the error start and end
+            if line_num == error_start_line {
+                error_start_char = line_starting_pos + self.area.start().char();
+            }
+            if line_num == error_end_line {
+                error_end_char = line_starting_pos + line.len() + self.area.end().char();
+            }
+            // Where the error begins and where it ends in the current line
+            // Used for making the text white or yellow
+            #[allow(clippy::comparison_chain)]
+            // Lines without annotations always have the lf selected
+            let line_error_start_char = if line_num < error_start_line || line_num > error_end_line
+            {
+                line.len()
+            }
+            //Before the error lines, so it doesn't begin at all
+            else if line_num == error_start_line {
+                self.area.start().char()
+            } else {
+                0
+            };
+
+            // Using a match makes this hard to read
+            #[allow(clippy::comparison_chain)]
+            let line_error_end_char = if line_num > error_end_line {
+                // Lines without annotations always have the lf selected
+                line.len()
+            }
+            //After the error lines, so it end immediately
+            else if line_num == error_end_line {
+                self.area.end().char()
+            } else {
+                line.len()
+            };
+
+            // Add the line
+            let line_label = Label::new((
+                &self.file_location,
+                line_starting_pos..line_starting_pos + line.len(),
+            ))
+                .with_color(code_color);
+            // Mark the error
+            let mut error_label = Label::new((
+                &self.file_location,
+                line_starting_pos + line_error_start_char..line_starting_pos + line_error_end_char,
+            ))
+                .with_priority(1)
+                .with_color(error_code_color);
+            // Add the error message
+            if line_num == self.area.end().line() {
+                error_label =
+                    error_label.with_message((&self.error_msg).fg(error_msg_color))
+            }
+            lines.push(line_label);
+            lines.push(error_label);
+            Self::update_line_starting_pos(&self.code, &mut line_starting_pos);
+        }
+
+        let report = Report::build(
+            self.error_type.as_report_kind(),
+            (&self.file_location, error_start_char..error_end_char),
+        )
+            .with_message(self.error_type.as_msg().fg(syntax_error_color))
+            .with_labels(lines);
+        // Prints Error and the error message
+        report
+            .finish()
+            .print((&self.file_location, Source::from(&self.code)))
+            .unwrap();
+    }
+
+    fn update_line_starting_pos(code: &str, line_starting_pos: &mut usize) {
+        if let Some(new_start) = advance_till_next_line(code, *line_starting_pos) {
+            *line_starting_pos = new_start;
+        }
     }
 }
 
@@ -55,20 +172,24 @@ Expects the user to set every field before building
 pub struct SyntaxErrorBuilder<
     AreaStatus: AttributeStatus<CodeArea>,
     FileLocationStatus: AttributeStatus<String>,
-    ErrorTypeStatus: AttributeStatus<Box<dyn ErrorType>>,
+    ErrorTypeStatus: AttributeStatus<ErrorType>,
+    ErrorMsgStatus: AttributeStatus<String>,
+    CodeStatus: AttributeStatus<String>,
 > {
     area: AreaStatus::Attribute,
     file_location: FileLocationStatus::Attribute,
     error_type: ErrorTypeStatus::Attribute,
+    error_msg: ErrorMsgStatus::Attribute,
+    code: CodeStatus::Attribute
 }
 
-impl Default for SyntaxErrorBuilder<NotSet, NotSet, NotSet> {
+impl Default for SyntaxErrorBuilder<NotSet, NotSet, NotSet, NotSet, NotSet> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl SyntaxErrorBuilder<NotSet, NotSet, NotSet> {
+impl SyntaxErrorBuilder<NotSet, NotSet, NotSet, NotSet, NotSet> {
     /** Creates a new and empty [`SyntaxErrorBuilder`]
      */
     fn new() -> Self {
@@ -76,31 +197,42 @@ impl SyntaxErrorBuilder<NotSet, NotSet, NotSet> {
             area: (),
             file_location: (),
             error_type: (),
+            error_msg: (),
+            code: ()
         }
     }
 }
 
 impl<
     FileLocationStatus: AttributeStatus<String>,
-    ErrorTypeStatus: AttributeStatus<Box<dyn ErrorType>>,
-> SyntaxErrorBuilder<NotSet, FileLocationStatus, ErrorTypeStatus>
+    ErrorTypeStatus: AttributeStatus<ErrorType>,
+    ErrorMsgStatus: AttributeStatus<String>,
+    CodeStatus: AttributeStatus<String>,
+> SyntaxErrorBuilder<NotSet, FileLocationStatus, ErrorTypeStatus, ErrorMsgStatus, CodeStatus>
 {
     /** Sets an error area
      */
     pub fn with_area(
         self,
         area: CodeArea,
-    ) -> SyntaxErrorBuilder<Set, FileLocationStatus, ErrorTypeStatus> {
+    ) -> SyntaxErrorBuilder<Set, FileLocationStatus, ErrorTypeStatus, ErrorMsgStatus, CodeStatus> {
         SyntaxErrorBuilder {
             area,
             file_location: self.file_location,
             error_type: self.error_type,
+            error_msg: self.error_msg,
+            code: self.code
         }
     }
 }
 
-impl<AreaStatus: AttributeStatus<CodeArea>, ErrorTypeStatus: AttributeStatus<Box<dyn ErrorType>>>
-    SyntaxErrorBuilder<AreaStatus, NotSet, ErrorTypeStatus>
+impl<
+    AreaStatus: AttributeStatus<CodeArea>,
+    ErrorTypeStatus: AttributeStatus<ErrorType>,
+    ErrorMsgStatus: AttributeStatus<String>,
+    CodeStatus: AttributeStatus<String>,
+>
+    SyntaxErrorBuilder<AreaStatus, NotSet, ErrorTypeStatus, ErrorMsgStatus, CodeStatus>
 {
     /** Sets the path of the file that contains the erroneous code
           This is for display purposes only
@@ -108,39 +240,95 @@ impl<AreaStatus: AttributeStatus<CodeArea>, ErrorTypeStatus: AttributeStatus<Box
     pub fn with_file_location(
         self,
         file_location: String,
-    ) -> SyntaxErrorBuilder<AreaStatus, Set, ErrorTypeStatus> {
+    ) -> SyntaxErrorBuilder<AreaStatus, Set, ErrorTypeStatus, ErrorMsgStatus, CodeStatus> {
         SyntaxErrorBuilder {
             area: self.area,
             file_location,
             error_type: self.error_type,
+            error_msg: self.error_msg,
+            code: self.code
         }
     }
 }
 
-impl<AreaStatus: AttributeStatus<CodeArea>, FileLocationStatus: AttributeStatus<String>>
-    SyntaxErrorBuilder<AreaStatus, FileLocationStatus, NotSet>
+impl<
+    AreaStatus: AttributeStatus<CodeArea>,
+    FileLocationStatus: AttributeStatus<String>,
+    ErrorMsgStatus: AttributeStatus<String>,
+    CodeStatus: AttributeStatus<String>,
+>
+    SyntaxErrorBuilder<AreaStatus, FileLocationStatus, NotSet, ErrorMsgStatus, CodeStatus>
 {
     /** Sets the type of error
      */
     pub fn with_error_type(
         self,
-        error_type: impl ErrorType + 'static,
-    ) -> SyntaxErrorBuilder<AreaStatus, FileLocationStatus, Set> {
+        error_type: ErrorType,
+    ) -> SyntaxErrorBuilder<AreaStatus, FileLocationStatus, Set, ErrorMsgStatus, CodeStatus> {
         SyntaxErrorBuilder {
             area: self.area,
             file_location: self.file_location,
-            error_type: Box::new(error_type),
+            error_type,
+            error_msg: self.error_msg,
+            code: self.code
         }
     }
 }
 
-impl SyntaxErrorBuilder<Set, Set, Set> {
+impl<
+    AreaStatus: AttributeStatus<CodeArea>,
+    FileLocationStatus: AttributeStatus<String>,
+    ErrorTypeStatus: AttributeStatus<ErrorType>,
+    CodeStatus: AttributeStatus<String>,
+>
+SyntaxErrorBuilder<AreaStatus, FileLocationStatus, ErrorTypeStatus, NotSet, CodeStatus>
+{
+    /** Sets the error message
+     */
+    pub fn with_error_msg(
+        self,
+        error_msg: String,
+    ) -> SyntaxErrorBuilder<AreaStatus, FileLocationStatus, ErrorTypeStatus, Set, CodeStatus> {
+        SyntaxErrorBuilder {
+            area: self.area,
+            file_location: self.file_location,
+            error_type: self.error_type,
+            error_msg,
+            code: self.code
+        }
+    }
+}
+
+impl<
+    AreaStatus: AttributeStatus<CodeArea>,
+    FileLocationStatus: AttributeStatus<String>,
+    ErrorTypeStatus: AttributeStatus<ErrorType>,
+    ErrorMsgStatus: AttributeStatus<String>,
+>
+SyntaxErrorBuilder<AreaStatus, FileLocationStatus, ErrorTypeStatus, ErrorMsgStatus, NotSet>
+{
+    /** Sets the error message
+     */
+    pub fn with_code(
+        self,
+        code: String,
+    ) -> SyntaxErrorBuilder<AreaStatus, FileLocationStatus, ErrorTypeStatus, ErrorMsgStatus, Set> {
+        SyntaxErrorBuilder {
+            area: self.area,
+            file_location: self.file_location,
+            error_type: self.error_type,
+            error_msg: self.error_msg,
+            code
+        }
+    }
+}
+impl SyntaxErrorBuilder<Set, Set, Set, Set, Set> {
     /** Builds the [`SyntaxError`]
     # Panics
     Panics if not all fields are set
     */
     pub fn build(self) -> SyntaxError {
-        SyntaxError::new(self.area, self.file_location, self.error_type)
+        SyntaxError::new(self.area, self.file_location, self.error_type, self.error_msg, self.code)
     }
 }
 
@@ -199,121 +387,6 @@ impl CodeArea {
     }
 }
 
-impl SyntaxError {
-    /** Prints the error to stdout
-    # Panic
-    May panic if the area is referencing to locations that don't exist in the the provided code
-    */
-    pub fn print(&self, code: &str) {
-        // Orange
-        let error_code_color = Color::BrightWhite;
-        let error_msg_color = Color::Red;
-        let syntax_error_color = Color::BrightWhite;
-        let code_color = Color::Primary;
-
-        let mut line_starting_pos = 0;
-        // The start and end
-        let error_start_line = self.area.start().line();
-        let error_end_line = self.area.end().line();
-        // The start and end including the padding
-        let display_end = error_end_line + ERROR_CONTEXT_LINES;
-        let display_start = if error_start_line < ERROR_CONTEXT_LINES {
-            0
-        } else {
-            error_start_line - ERROR_CONTEXT_LINES
-        };
-
-        // The processed lines with annotations
-        let mut lines = Vec::new();
-        // Where the error begins, relative to the start of code
-        let mut error_start_char = 0;
-        let mut error_end_char = 0;
-        // Process the codelines
-        for (line_num, line) in code.lines().enumerate() {
-            // If the code line is out of the window to print, skip it
-            if line_num < display_start || line_num > display_end {
-                //
-                Self::update_line_starting_pos(code, &mut line_starting_pos);
-                continue;
-            }
-            // Update the error start and end
-            if line_num == error_start_line {
-                error_start_char = line_starting_pos + self.area.start().char();
-            }
-            if line_num == error_end_line {
-                error_end_char = line_starting_pos + line.len() + self.area.end().char();
-            }
-            // Where the error begins and where it ends in the current line
-            // Used for making the text white or yellow
-            #[allow(clippy::comparison_chain)]
-            // Lines without annotations always have the lf selected
-            let line_error_start_char = if line_num < error_start_line || line_num > error_end_line
-            {
-                line.len()
-            }
-            //Before the error lines, so it doesn't begin at all
-            else if line_num == error_start_line {
-                self.area.start().char()
-            } else {
-                0
-            };
-
-            // Using a match makes this hard to read
-            #[allow(clippy::comparison_chain)]
-            let line_error_end_char = if line_num > error_end_line {
-                // Lines without annotations always have the lf selected
-                line.len()
-            }
-            //After the error lines, so it end immediately
-            else if line_num == error_end_line {
-                self.area.end().char()
-            } else {
-                line.len()
-            };
-
-            // Add the line
-            let line_label = Label::new((
-                &self.file_location,
-                line_starting_pos..line_starting_pos + line.len(),
-            ))
-            .with_color(code_color);
-            // Mark the error
-            let mut error_label = Label::new((
-                &self.file_location,
-                line_starting_pos + line_error_start_char..line_starting_pos + line_error_end_char,
-            ))
-            .with_priority(1)
-            .with_color(error_code_color);
-            // Add the error message
-            if line_num == self.area.end().line() {
-                error_label =
-                    error_label.with_message(self.error_type.to_string().fg(error_msg_color))
-            }
-            lines.push(line_label);
-            lines.push(error_label);
-            Self::update_line_starting_pos(code, &mut line_starting_pos);
-        }
-
-        let report = Report::build(
-            ReportKind::Custom("Error", error_msg_color),
-            (&self.file_location, error_start_char..error_end_char),
-        )
-        .with_message("A syntax error was found during compilation".fg(syntax_error_color))
-        .with_labels(lines);
-        // Prints Error and the error message
-        report
-            .finish()
-            .print((&self.file_location, Source::from(code)))
-            .unwrap();
-    }
-
-    fn update_line_starting_pos(code: &str, line_starting_pos: &mut usize) {
-        if let Some(new_start) = advance_till_next_line(code, *line_starting_pos) {
-            *line_starting_pos = new_start;
-        }
-    }
-}
-
 fn advance_till_next_line(to_advance: &str, current_index: usize) -> Option<usize> {
     let mut found_lf = false;
     let mut found_cr = false;
@@ -333,44 +406,63 @@ fn advance_till_next_line(to_advance: &str, current_index: usize) -> Option<usiz
     None
 }
 
-pub trait ErrorType: Debug {
-    fn to_string(&self) -> String;
+#[derive(Debug)]
+pub enum ErrorType {
+    Error,
+    Warning,
+    Info
+}
+
+impl ErrorType
+{
+    fn as_report_kind(&self) -> ReportKind
+    {
+        match self
+        {
+            ErrorType::Error => ReportKind::Custom("Error", Color::Red),
+            ErrorType::Warning => ReportKind::Warning,
+            ErrorType::Info => ReportKind::Advice
+        }
+    }
+
+    fn as_msg(&self) -> String
+    {
+        format!("A{} was found during compilation", match self
+        {
+            ErrorType::Error => " syntax error",
+            ErrorType::Warning => " warning",
+            ErrorType::Info => "n information"
+        })
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{CodeArea, CodeLocation, ErrorType, SyntaxError};
-
-    #[derive(Debug)]
-    pub struct ExampleError(String);
-    impl ErrorType for ExampleError {
-        fn to_string(&self) -> String {
-            format!(
-                "\"{}\" is an invalid data type at this point in the programm",
-                self.0
-            )
-        }
-    }
     #[test]
     fn error() {
         let error = SyntaxError::builder()
             .with_area(CodeArea::new(CodeLocation::new(12, 10), CodeLocation::new(12, 18)).unwrap())
-            .with_error_type(ExampleError("CodeArea".to_string()))
+            .with_error_type(ErrorType::Error)
+            .with_error_msg("CodeArea is an invalid data type at this point in the programm".to_string())
             .with_file_location("main.waso".to_string())
+            .with_code(include_str!("lib.rs").to_string())
             .build();
 
-        error.print(include_str!("lib.rs"));
+        error.print();
     }
 
     #[test]
     fn error_multiline() {
         let error = SyntaxError::builder()
             .with_area(CodeArea::new(CodeLocation::new(6, 0), CodeLocation::new(8, 2)).unwrap())
-            .with_error_type(ExampleError("CodeArea".to_string()))
+            .with_error_type(ErrorType::Error)
+            .with_error_msg("CodeArea is an invalid data type at this point in the programm".to_string())
             .with_file_location("main.waso".to_string())
+            .with_code(include_str!("lib.rs").to_string())
             .build();
 
-        error.print(include_str!("lib.rs"));
+        error.print();
     }
 
     #[test]
