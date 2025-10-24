83dc67c104023238a3c48b738e2a7175d8cff99e
Fant3sy <werker.turk@gmail.com>
2025-10-24 22:19:39 +0200

    started to work on the statements

--- compiler/middle_end/semantic_analyzer/src/lib.rs
+++ compiler/middle_end/semantic_analyzer/src/lib.rs
@@ -2,6 +2,7 @@ mod expression_sa;
 mod mics_sa;
 mod statement_sa;
 mod top_level_sa;
+mod symbol_mapper;
 
 use ast::{AST, TypedAST, UntypedAST};
 
--- compiler/middle_end/semantic_analyzer/src/statement_sa.rs
+++ compiler/middle_end/semantic_analyzer/src/statement_sa.rs
@@ -1,6 +1,29 @@
-use ast::statement::Statement;
+use ast::statement::{Return, Statement};
 use ast::{TypedAST, UntypedAST};
+use ast::expression::Expression;
+use ast::traversal::statement_traversal::StatementTraversalHelper;
+use crate::expression_sa;
+use crate::expression_sa::analyze_expression;
+use crate::symbol_mapper::SymbolMapper;
 
-pub(crate) fn analyze_expression(to_analyze: Statement<UntypedAST>) -> Option<Statement<TypedAST>> {
+pub(crate) fn analyze_statement(to_analyze: StatementTraversalHelper<UntypedAST>, symbol_mapper: &mut SymbolMapper) -> Option<Statement<TypedAST>> {
+    match  to_analyze.get_inner() {
+        Statement::VariableAssignment(_) => todo!(),
+        Statement::VariableDeclaration(_) => todo!(),
+        Statement::Expression(inner) => {
+            let typed_expr = analyze_expression(inner, symbol_mapper)?;
+            Some(Statement::Expression(typed_expr))
+        }
+        Statement::Return(inner) => {
+        let typed_return = analyze_return(inner,symbol_mapper)?;
+        Some(Statement::Return(typed_return))
+        },
+        Statement::ControlStructure(_) => todo!(), //3
+        Statement::Codeblock(_) => todo!(), //4
+        Statement::VoidFunctionCall(_) => todo!()
+    }
+}
+
+fn analyze_return(to_analyze : &Return<UntypedAST>,symbol_mapper: &mut SymbolMapper) -> Option<Return<TypedAST>>{
     todo!()
 }
