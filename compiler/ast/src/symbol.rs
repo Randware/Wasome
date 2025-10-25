c813413426f5b150b4c587ae2e34066c3b1ebda6
Running-Lag <131605267+Running-Lag@users.noreply.github.com>
2025-10-25 20:11:37 +0200

    Used cargo fmt to use the official rust formatting style

--- compiler/ast/src/symbol.rs
+++ compiler/ast/src/symbol.rs
@@ -139,31 +139,40 @@ impl FunctionCall<UntypedAST> {
 }
 
 #[cfg(test)]
-mod tests
-{
-    use std::rc::Rc;
+mod tests {
+    use crate::data_type::DataType;
     use crate::expression::{Expression, Literal};
     use crate::symbol::{FunctionCall, FunctionSymbol, VariableSymbol};
-    use crate::{ASTNode, SemanticEquality, TypedAST, UntypedAST};
-    use crate::data_type::DataType;
     use crate::test_shared::sample_codearea;
+    use crate::{ASTNode, SemanticEquality, TypedAST, UntypedAST};
+    use std::rc::Rc;
 
     #[test]
-    fn create_function_call_untyped()
-    {
+    fn create_function_call_untyped() {
         let name = "test".to_string();
-        let arg = ASTNode::new(Expression::<UntypedAST>::Literal("10".to_string()), sample_codearea());
+        let arg = ASTNode::new(
+            Expression::<UntypedAST>::Literal("10".to_string()),
+            sample_codearea(),
+        );
         let call = FunctionCall::<UntypedAST>::new(name, vec![arg]);
         assert_eq!("test", call.function());
         assert_eq!(1, call.args().len());
-
     }
 
     #[test]
-    fn create_function_call_typed_wrong_args()
-    {
-        let symbol = Rc::new(FunctionSymbol::new("test".to_string(), None, vec![Rc::new(VariableSymbol::new("test1".to_string(), DataType::Bool))]));
-        let arg = ASTNode::new(Expression::<TypedAST>::Literal(Literal::S32(10)), sample_codearea());
+    fn create_function_call_typed_wrong_args() {
+        let symbol = Rc::new(FunctionSymbol::new(
+            "test".to_string(),
+            None,
+            vec![Rc::new(VariableSymbol::new(
+                "test1".to_string(),
+                DataType::Bool,
+            ))],
+        ));
+        let arg = ASTNode::new(
+            Expression::<TypedAST>::Literal(Literal::S32(10)),
+            sample_codearea(),
+        );
         let call = FunctionCall::<TypedAST>::new(symbol.clone(), vec![arg]);
         assert_eq!(None, call);
 
@@ -172,15 +181,27 @@ mod tests
     }
 
     #[test]
-    fn create_function_call_typed()
-    {
-        let symbol = Rc::new(FunctionSymbol::new("test".to_string(), None, vec![Rc::new(VariableSymbol::new("test1".to_string(), DataType::Bool))]));
-        let arg = ASTNode::new(Expression::<TypedAST>::Literal(Literal::Bool(true)), sample_codearea());
+    fn create_function_call_typed() {
+        let symbol = Rc::new(FunctionSymbol::new(
+            "test".to_string(),
+            None,
+            vec![Rc::new(VariableSymbol::new(
+                "test1".to_string(),
+                DataType::Bool,
+            ))],
+        ));
+        let arg = ASTNode::new(
+            Expression::<TypedAST>::Literal(Literal::Bool(true)),
+            sample_codearea(),
+        );
         let call = FunctionCall::<TypedAST>::new(symbol.clone(), vec![arg]);
         assert_eq!(None, call.as_ref().unwrap().function().return_type());
         assert_eq!("test", call.as_ref().unwrap().function().name());
 
-        let arg2 = ASTNode::new(Expression::<TypedAST>::Literal(Literal::Bool(true)), sample_codearea());
+        let arg2 = ASTNode::new(
+            Expression::<TypedAST>::Literal(Literal::Bool(true)),
+            sample_codearea(),
+        );
         let call2 = FunctionCall::<TypedAST>::new(symbol.clone(), vec![arg2]);
         assert!(call.semantic_equals(&call2));
     }
