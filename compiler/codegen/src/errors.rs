4d9f397eb24f426cf6955eb031ea8f684693a86d
Dari-OS <darius.dev@proton.me>
2026-01-13 00:06:08 +0100

    Add backend errors
    Even though they are not necessary, since the crates that manage the AST are
    responsible for handling these types of errors (except the ICE error).
    It's still a good way to debug the code easier when an error occurs.

---
+++ compiler/codegen/src/errors.rs
@@ -0,0 +1,24 @@
+use source::types::Span;
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum CodegenError<'src> {
+    #[error("Unknown variable '{0}'")]
+    UnknownVariable(&'src str, Span),
+
+    #[error("Unknown function '{0}'")]
+    UnknownFunction(&'src str, Span),
+
+    #[error("Main function not found")]
+    MissingMainFunction,
+
+    #[error("Entry block definition failed")]
+    BlockCreationFailure,
+
+    #[error("Feature '{0}' is not yet implemented in backend")]
+    Unimplemented(&'static str, Span),
+
+    #[error("Internal Compiler Error: {0}")]
+    /// *Internal Compiler Error*
+    Ice(String),
+}
