194040079484d60958d14ba3022aaaeda2e0de06
Dari-OS <darius.dev@proton.me>
2026-01-13 01:18:02 +0100

    Add codegen types

--- compiler/codegen/src/lib.rs
+++ compiler/codegen/src/lib.rs
@@ -1,4 +1,6 @@
 mod context;
+mod errors;
 mod generators;
 mod memory;
 mod temp;
+mod types;
--- compiler/codegen/src/main.rs
+++ compiler/codegen/src/main.rs
@@ -2,9 +2,11 @@ use inkwell::context::Context;
 use temp::Cli;
 
 mod context;
+mod errors;
 mod generators;
 mod memory;
 mod temp;
+mod types;
 
 fn main() {
     let config = Cli::mockup();
