36e3320d2e6951743ba0303cea2f662c9dae1f2c
Dari-OS <darius.dev@proton.me>
2026-01-25 04:21:42 +0100

    Add builder pattern via bon

--- compiler/codegen/src/lib.rs
+++ compiler/codegen/src/lib.rs
@@ -4,3 +4,33 @@ mod generators;
 mod memory;
 mod temp;
 mod types;
+use std::path::PathBuf;
+
+use ast::{AST, TypedAST};
+use bon::bon;
+use inkwell::context::Context;
+
+use crate::{context::LLVMContext, types::OptLevel};
+
+pub struct Codegen<'ctx> {
+    ast: AST<TypedAST>,
+    context: LLVMContext<'ctx>,
+    output: PathBuf,
+}
+
+#[bon]
+impl<'ctx> Codegen<'ctx> {
+    #[builder]
+    pub fn new(
+        context: &'ctx Context,
+        ast: AST<TypedAST>,
+        #[builder(default = OptLevel::O0)] opt_level: OptLevel,
+        output: PathBuf,
+    ) -> Self {
+        Self {
+            ast,
+            context: LLVMContext::new(context, opt_level),
+            output,
+        }
+    }
+}
