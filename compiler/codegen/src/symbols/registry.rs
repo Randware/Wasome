f7a53bcef8cab2ddc42de7294c4f426358bc9490
Dari-OS <darius.dev@proton.me>
2026-03-15 16:47:25 +0100

    Add enums to type registry

--- compiler/codegen/src/symbols/registry.rs
+++ compiler/codegen/src/symbols/registry.rs
@@ -4,13 +4,15 @@ use ast::{TypedAST, symbol::StructSymbol};
 use inkwell::{context::Context, types::StructType};
 
 pub struct TypeRegistry<'ctx> {
-    registry: HashMap<Rc<StructSymbol<TypedAST>>, StructType<'ctx>>,
+    structs: HashMap<Rc<StructSymbol<TypedAST>>, StructType<'ctx>>,
+    enums: HashMap<Rc<StructSymbol<TypedAST>>, StructType<'ctx>>,
 }
 
 impl<'ctx> TypeRegistry<'ctx> {
     pub fn new() -> Self {
         Self {
-            registry: HashMap::new(),
+            structs: HashMap::new(),
+            enums: HashMap::new(),
         }
     }
 
@@ -19,10 +21,22 @@ impl<'ctx> TypeRegistry<'ctx> {
         symbol: Rc<StructSymbol<TypedAST>>,
         struct_type: StructType<'ctx>,
     ) -> Option<StructType<'ctx>> {
-        self.registry.insert(symbol, struct_type)
+        self.structs.insert(symbol, struct_type)
     }
 
     pub fn get_struct(&self, symbol: &Rc<StructSymbol<TypedAST>>) -> Option<StructType<'ctx>> {
-        self.registry.get(symbol).copied()
+        self.structs.get(symbol).copied()
+    }
+
+    pub fn register_enum(
+        &mut self,
+        symbol: Rc<StructSymbol<TypedAST>>,
+        struct_type: StructType<'ctx>,
+    ) -> Option<StructType<'ctx>> {
+        self.structs.insert(symbol, struct_type)
+    }
+
+    pub fn get_enum(&self, symbol: &Rc<StructSymbol<TypedAST>>) -> Option<StructType<'ctx>> {
+        self.structs.get(symbol).copied()
     }
 }
