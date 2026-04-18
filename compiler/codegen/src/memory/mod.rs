8296decb91d7363c2a29c56e8b69fc524ea26be1
Running-Lag <131605267+Running-Lag@users.noreply.github.com>
2026-04-18 17:15:44 +0200

    More refactoring

--- compiler/codegen/src/memory/mod.rs
+++ compiler/codegen/src/memory/mod.rs
@@ -1,39 +1,93 @@
-use crate::Codegen;
 use crate::context::{FunctionContext, LLVMContext};
-use ast::TypedAST;
+use crate::Codegen;
 use ast::data_type::DataType;
 use ast::symbol::{EnumSymbol, StructSymbol};
-use inkwell::values::{BasicValue, BasicValueEnum, FunctionValue, PointerValue};
+use ast::TypedAST;
+use inkwell::values::{BasicValue, BasicValueEnum, FunctionValue, IntValue, PointerValue};
 use inkwell::{AddressSpace, IntPredicate};
 
 impl<'ctx> Codegen<'ctx> {
-    pub(crate) fn compile_inc_refcount(
+    fn read_refcount(
         &self,
         llvm_context: &LLVMContext<'ctx>,
-        to_generate: PointerValue<'ctx>,
+        pointer: PointerValue<'ctx>,
+    ) -> IntValue<'ctx> {
+        let refc = llvm_context
+            .builder()
+            .build_struct_gep(
+                llvm_context.global_registry().base_heap_allocated(),
+                pointer,
+                0,
+                "gep_refc",
+            )
+            .unwrap();
+        llvm_context
+            .builder()
+            .build_load(self.context.i32_type(), refc, "load_refc")
+            .unwrap()
+            .into_int_value()
+    }
+
+    fn write_refcount(
+        llvm_context: &LLVMContext<'ctx>,
+        pointer: PointerValue<'ctx>,
+        value: IntValue<'ctx>,
     ) {
         let refc = llvm_context
             .builder()
             .build_struct_gep(
                 llvm_context.global_registry().base_heap_allocated(),
-                to_generate,
+                pointer,
                 0,
-                "gep_refc_inc",
+                "gep_refc",
             )
             .unwrap();
-        let refc_val = llvm_context
-            .builder()
-            .build_load(self.context.i32_type(), refc, "load_refc_inc")
-            .unwrap();
-        let inc = llvm_context
+        llvm_context.builder().build_store(refc, value).unwrap();
+    }
+
+    fn compile_add_refcount(
+        &self,
+        llvm_context: &LLVMContext<'ctx>,
+        pointer: PointerValue<'ctx>,
+        amount: u32,
+    ) {
+        let current = self.read_refcount(llvm_context, pointer);
+        let result = llvm_context
             .builder()
             .build_int_add(
-                refc_val.into_int_value(),
-                self.context.i32_type().const_int(1, false),
-                "inc_refc",
+                current,
+                self.context.i32_type().const_int(u64::from(amount), false),
+                "add_refc",
             )
             .unwrap();
-        llvm_context.builder().build_store(refc, inc).unwrap();
+        Self::write_refcount(llvm_context, pointer, result);
+    }
+
+    fn compile_sub_refcount(
+        &self,
+        llvm_context: &LLVMContext<'ctx>,
+        pointer: PointerValue<'ctx>,
+        amount: u32,
+    ) -> IntValue<'ctx> {
+        let current = self.read_refcount(llvm_context, pointer);
+        let result = llvm_context
+            .builder()
+            .build_int_sub(
+                current,
+                self.context.i32_type().const_int(u64::from(amount), false),
+                "sub_refc",
+            )
+            .unwrap();
+        Self::write_refcount(llvm_context, pointer, result);
+        result
+    }
+
+    pub(crate) fn compile_inc_refcount(
+        &self,
+        llvm_context: &LLVMContext<'ctx>,
+        to_generate: PointerValue<'ctx>,
+    ) {
+        self.compile_add_refcount(llvm_context, to_generate, 1);
     }
 
     pub(crate) fn compile_val_ref_drop(
@@ -128,28 +182,7 @@ impl<'ctx> Codegen<'ctx> {
         on_drop: FunctionValue<'ctx>,
         to_generate: PointerValue<'ctx>,
     ) {
-        let refc = llvm_context
-            .builder()
-            .build_struct_gep(
-                llvm_context.global_registry().base_heap_allocated(),
-                to_generate,
-                0,
-                "gep_refc_dec",
-            )
-            .unwrap();
-        let refc_val = llvm_context
-            .builder()
-            .build_load(self.context.i32_type(), refc, "load_refc_dec")
-            .unwrap();
-        let dec = llvm_context
-            .builder()
-            .build_int_sub(
-                refc_val.into_int_value(),
-                self.context.i32_type().const_int(1, false),
-                "dec_refc",
-            )
-            .unwrap();
-        llvm_context.builder().build_store(refc, dec).unwrap();
+        let dec = self.compile_sub_refcount(llvm_context, to_generate, 1);
 
         let current_function = func.current_function();
         let drop_bb = llvm_context
@@ -219,8 +252,7 @@ impl<'ctx> Codegen<'ctx> {
 
                 // Set the refcount to 2
                 // This way, it can never be dropped again and can't cause infinite loops
-                self.compile_inc_refcount(llvm_context, to_generate);
-                self.compile_inc_refcount(llvm_context, to_generate);
+                self.compile_add_refcount(llvm_context, to_generate, 2);
                 llvm_context
                     .builder()
                     .build_call(
@@ -230,19 +262,7 @@ impl<'ctx> Codegen<'ctx> {
                     )
                     .unwrap();
 
-                let refc = llvm_context
-                    .builder()
-                    .build_struct_gep(
-                        llvm_context.global_registry().base_heap_allocated(),
-                        to_generate,
-                        0,
-                        "gep_refc_dec",
-                    )
-                    .unwrap();
-                let refc_val = llvm_context
-                    .builder()
-                    .build_load(self.context.i32_type(), refc, "load_refc_dec")
-                    .unwrap();
+                let refc_val = self.read_refcount(llvm_context, to_generate);
 
                 llvm_context
                     .builder()
@@ -251,7 +271,7 @@ impl<'ctx> Codegen<'ctx> {
                             .builder()
                             .build_int_compare(
                                 IntPredicate::EQ,
-                                refc_val.into_int_value(),
+                                refc_val,
                                 llvm_context.context().i32_type().const_int(1, false),
                                 "cond",
                             )
