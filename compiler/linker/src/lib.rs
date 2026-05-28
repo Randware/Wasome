dbc53be0c289cb074daad4187579a87254d3e521
Running-Lag <131605267+Running-Lag@users.noreply.github.com>
2026-05-28 11:18:59 +0200

    Added read_line_internal

--- compiler/linker/src/lib.rs
+++ compiler/linker/src/lib.rs
@@ -114,8 +114,11 @@ impl Linker<Ready> {
         output.args(["-flavor", "wasm", "--export-if-defined=__externref_table_alloc",
             "--export-if-defined=__wbindgen_describe___wbg___wbindgen_throw_1506f2235d1bdba0",
             "--export-if-defined=__wbindgen_describe___wbg_print_str_65ceab8bbf81d4ae",
+            "--export-if-defined=__wbindgen_describe___wbg_read_line_internal_6c842bcd91b36a7e",
             "--export-if-defined=print_string",
+            "--export-if-defined=read_line_internal",
             "--export-if-defined=__wbindgen_describe_main",
+            "--export-if-defined=__wbindgen_malloc",
             "--export-if-defined=__externref_table_dealloc",
             "--export-if-defined=main"
 
